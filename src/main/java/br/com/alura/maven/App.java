7aa0f1cd87113d114a668a77a4af3ec625227bcd
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2019-12-24 14:15:13 -0300

    AULA04 - FAses do maven - Ajustando codigo fonte

--- src/main/java/br/com/alura/maven/App.java
+++ src/main/java/br/com/alura/maven/App.java
@@ -6,8 +6,10 @@ package br.com.alura.maven;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) { 
+
+        Produto produto = new Produto("Bala juquinha sabor tangerina", 0.15);
+
+        System.out.println("A bala que eu gosto é: "+ produto.getNome() + "e custa " + produto.getPreco());
     }
 }
